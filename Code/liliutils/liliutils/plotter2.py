a389e8514a06d5756fa7946678a8bd80c3dcb505
Stanislav <stanislav@pikkatech.eu>
2026-01-12 13:17:55 +0100

    Code\liliutils\liliutils\plotter2.py

--- Code/liliutils/liliutils/plotter2.py
+++ Code/liliutils/liliutils/plotter2.py
@@ -34,23 +34,52 @@ DEFAULT_CURVE_WIDTH     = 1.5
 #endregion
 
 class Plotter:
+    """
+    Provides a tool to plot curve graphs.
+    """
     def __init__(self, **kwargs):
+        """
+        :param kwargs: Dictionary of plotting parameters.
+            Parameters:
+            ==========
+            'size'          Size of plot area, inch * inch. Default: (8, 8)
+            'min_x'         Minimum value of X (X-axis). Default: 0
+            'max_x'         Maximum value of X (X-axis). Default: 1
+            'major_step_x'  Major grid step on X axis. Default: 0.1
+            'minor_step_x'  Minor grid step on X axis. Default: 0.05
+            'digits_x'      Number of digits for the characters on X axis. Default: 1
+            'min_y'         Minimum value of Y (Y-axis). Default: 0
+            'max_y'         Maximum value of Y (Y-axis). Default: 1
+            'major_step_y'  Major grid step on Y axis. Default: 0.1
+            'minor_step_y'  Minor grid step on Y axis. Default: 0.05
+            'digits_y'      Number of digits for the characters on Y axis. Default: 1
+            'label_x'       Label of the X axis. Default: 'X'.
+            'label_y'       Label of the Y axis. Default: 'Y'.
+            'title'         The title of the diagram. Default: empty string.
+        :return: None
+        """
         self._parameters = kwargs
         self._set_defaults()
         self._prepare_canvas()
 
     def plot(self, curves = list[Curve]):
+        """
+        Plots curve graphs for a list of curves.
+        :param curves: The list of curves.
+        :return: None
+        """
         for curve in curves:
             x = [t[0] for t in curve.values]
             y = [t[1] for t in curve.values]
 
             self._ax.plot(x, y, linewidth=self._parameters['curve_width'], label=curve.label, antialiased=True, color=curve.color)
-
+            # the next line is for scatter
+            # ax.scatter(x, y_noisy, color='r', marker='.', linewidth=0.5, label="theory", antialiased=True)
         self._ax.legend(loc='best')
 
         plt.show()
 
-
+    #region Protected auxiliary
     def _set_defaults(self):
         self._parameters.setdefault('size', DEFAULT_SIZE)
         self._parameters.setdefault('min_x', DEFAULT_MIN_X)
@@ -97,6 +126,7 @@ class Plotter:
 
         # Emphasize the x = 0 axis.
         ax.axhline(y=0, color='k', linewidth=0.5)
+    #endregion
 
 if __name__ == '__main__':
 
